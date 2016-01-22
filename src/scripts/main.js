218da6274c16f45a23389b654e92163fd61a13d2
Daniel Morgan <me@danielmorgan.co.uk>
2016-01-22 16:00:10 +0000

    Added fps counter and simple touch rotation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 var THREE = require('three');
+var $ = require('jquery');
+var Hammer = require('hammerjs');
+var Stats = require('stats-js');
 
-window.addEventListener('load', function() {
+var stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+
+var mc = new Hammer.Manager(window);
+mc.add(new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 0 }));
+
+$(function() {
+    $('body').append(stats.domElement);
+
     var width  = window.innerWidth;
     var height = window.innerHeight;
     var loader = new THREE.TextureLoader();
@@ -11,38 +24,49 @@ window.addEventListener('load', function() {
     var renderer = new THREE.WebGLRenderer({ antialiasing: true });
     var scene = new THREE.Scene();
     renderer.setSize(width, height);
-    renderer.setClearColor(0x222222, 1);
+    renderer.setClearColor(0x000000, 1);
+    document.body.appendChild(renderer.domElement);
 
     // Camera settings
     var camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 10000);
     camera.position.z = 25;
     camera.lookAt(scene.position);
 
-    var globe;
-
     // Build globe
+    var material = new THREE.MeshPhongMaterial();
+    var geometry = new THREE.SphereGeometry(5, 32, 32);
+    var globe = new THREE.Mesh(geometry, material);
     loader.load('../images/world.topo.bathy.200412.3x5400x2700.jpg', function(texture) {
-        var material = new THREE.MeshPhongMaterial({ map: texture });
-        var geometry = new THREE.SphereGeometry(5, 32, 32);
-        globe = new THREE.Mesh(geometry, material);
+        globe.material.map = texture;
         scene.add(globe);
-    })
+    });
 
     // Add light
-    var ambientLight = new THREE.AmbientLight(0xFFFFFF)
-    var sun = new THREE.DirectionalLight(0xFFFFFF, 1.3);
-    sun.position.set(-10, 0, 5);
+    var ambientLight = new THREE.AmbientLight(0x999999)
+    var sun = new THREE.DirectionalLight(0xFFDDBB, 2);
+    sun.position.set(-30, 10, 10);
     scene.add(ambientLight);
     scene.add(sun);
 
+    // Event binding
+    mc.on('pan', rotateGlobe);
+
     // Draw
     (function animate() {
-        requestAnimationFrame(animate);
+        setTimeout(function() {
+            stats.begin();
 
-        globe.rotation.y += 0.01
+            requestAnimationFrame(animate);
+            sun.position.x = Math.sin(Date.now() / 10000) * 30;
+            renderer.render(scene, camera);
 
-        sun.position.x = sun.position.x + 0.01;
-        renderer.render(scene, camera);
-        document.body.appendChild(renderer.domElement);
+            stats.end();
+        }, 1000 / 60)
     })();
+
+    // Input actions
+    function rotateGlobe(event) {
+        console.log(event.overallVelocityX);
+        globe.rotation.y += event.velocityX / 10;
+    }
 });
